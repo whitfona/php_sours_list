e2198435261d256bf1c4d52f212909e662f746db
Nick <nicholas@wealthyplanet.com>
2022-02-18 17:27:24 -0500

    change HOME path to /sours

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = '/sours';
 
     /**
      * The controller namespace for the application.
