36f65561ca95dc604c601c765cacbc34474d82ed
Nick <nicholas@wealthyplanet.com>
2022-02-20 17:19:25 -0500

    create seed data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Sour;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $user = User::factory()->create([
+            'name' => 'Nick',
+            'email' => 'whitford_4@hotmail.com',
+            'password' => Hash::make('Passw0rd!'),
+        ]);
+
+        //create 3 sours for user 1
+        Sour::factory(3)->create(['user_id' => $user->id]);
+        // create 10 other random sours
+        Sour::factory(10)->create();
     }
 }
