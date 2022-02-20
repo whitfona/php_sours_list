3452c089cb5f8847f6b01bb64eae29d0d88c41a3
Nick <nicholas@wealthyplanet.com>
2022-02-20 12:21:05 -0500

    format nav bar

--- resources/views/my-sours.blade.php
+++ resources/views/my-sours.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
     <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Sours') }}
+        <h2 class="font-semibold text-xl text-white leading-tight">
+            {{ __('My Sours') }}
         </h2>
     </x-slot>
 
