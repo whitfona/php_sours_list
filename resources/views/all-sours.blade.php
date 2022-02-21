b0be2db7a3d05f28b4a6cd87100eb91f50c55509
Nick <nicholas@wealthyplanet.com>
2022-02-21 10:47:15 -0500

    refactor css

--- resources/views/all-sours.blade.php
+++ resources/views/all-sours.blade.php
@@ -8,7 +8,7 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+                <div class="p-6">
                     @forelse($sours as $sour)
                         <div class="md:flex gap-4 mb-8">
                             <img class="md:max-w-xs" src="../images/beer.jpeg">
--- resources/views/my-sours.blade.php
+++ resources/views/my-sours.blade.php
@@ -30,7 +30,7 @@
     <div class="py-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-            <div class="p-6 bg-white border-b border-gray-200">
+            <div class="p-6">
                     @forelse($sours as $sour)
                         <div class="mb-8">
                             <div class="md:flex gap-4">
