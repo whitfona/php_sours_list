40270cf7d0527a5cc77143c87a4a9dadab07423f
Nick <nicholas@wealthyplanet.com>
2022-03-04 08:45:49 -0500

    add categories to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\Sour;
 use App\Models\User;
 use Illuminate\Database\Seeder;
@@ -16,13 +17,32 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        Category::factory()->create(['name' => 'Sour Beer']);
+        Category::factory()->create(['name' => 'Ale']);
+        Category::factory()->create(['name' => 'Cider']);
+        Category::factory()->create(['name' => 'Lager']);
+        Category::factory()->create(['name' => 'Porter']);
+        Category::factory()->create(['name' => 'Radler']);
+        Category::factory()->create(['name' => 'Stout']);
+        Category::factory()->create(['name' => 'Wheat Beer']);
+
         $user = User::factory()->create([
             'name' => 'Nick',
             'email' => 'whitford_4@hotmail.com',
             'password' => Hash::make('Passw0rd!'),
         ]);
 
-        Sour::factory(3)->create(['user_id' => $user->id]);
-        Sour::factory(10)->create();
+        Sour::factory(2)->create([
+            'user_id' => $user->id,
+            'category_id' => 1
+        ]);
+        Sour::factory(1)->create([
+            'user_id' => $user->id,
+            'category_id' => 2
+            ]);
+        Sour::factory(2)->create(['category_id' => 1]);
+        Sour::factory(5)->create(['category_id' => 5]);
+        Sour::factory(3)->create(['category_id' => 6]);
+
     }
 }
