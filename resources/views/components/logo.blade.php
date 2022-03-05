62c1d78262d4fd3a67de4b402c97fd6607e90228
Nick <nicholas@wealthyplanet.com>
2022-03-05 17:15:19 -0500

    Add new name, add favicon

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,4 +1,4 @@
 <a href="/" class="flex flex-col gap-2 justify-center items-center">
     <x-application-logo class="w-20 h-20 fill-current text-gray-500" />
-    <h1 class="text-3xl text-white">Sours App Name</h1>
+    <h1 class="text-3xl text-white">SHOUTS</h1>
 </a>
