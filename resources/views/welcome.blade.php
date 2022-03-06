b43979ddf7af686b74534ac4aa50f163242c1f05
Nick <nicholas@wealthyplanet.com>
2022-03-06 10:44:46 -0500

    style welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -38,7 +38,7 @@
                 <x-logo />
 
                 <div class="w-full bg-pink-400 sm:bg-white sm:max-w-md mt-6 px-6 py-4 bg-white overflow-hidden sm:rounded-lg">
-                    <div class="px-6 py-4 bg-white">
+                    <div class="px-6 py-4 bg-white rounded-lg">
                         <p>Have you ever been drinking with your pals and thought, <em>"Well how do you do, this certainly is a splendid tasting brew! I sure would like to wet my palate with this again."</em> But then proceeded to sample a few many cold ones that they all became a blur? Having experienced a similar situation, <strong>SHOUTS</strong> is here so that never happens again. Add, rate and share your bevvies, so you can enjoy your favourites and will never again have to suffer through a bevvie a previous you forgot you disliked.</p>
                     </div>
                 </div>
